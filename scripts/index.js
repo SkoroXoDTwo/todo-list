12f99915ff09a320768c983ce4859c063048a37e
Alexey Belkov <lexabelco2013@yandex.ru>
2022-10-06 15:11:50 +0500

    feat: реализовать возможность добовление новых задач

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 const mainSection = document.querySelector('.main');
 const taskListSection = document.querySelector('.task__list');
 const documentElement = document.documentElement;
+const formAddTask = document.querySelector('.task__form');
+const inputTextTask = formAddTask.querySelector('.task__textarea');
+const taskItemTemplate = document.querySelector('#task-list-option-template').content;
 let isDesctopPositionSection = () => document.documentElement.offsetWidth > 700;
 let getHeightScrollScreen = () => documentElement.scrollHeight - documentElement.offsetHeight;
+
 
 function changeHeightSectionTask () {
   mainSection.removeAttribute('style');
@@ -16,6 +20,21 @@ function changeHeightSectionTask () {
   }
 }
 
+function createTaskListItem (text) {
+  const listItem = taskItemTemplate.querySelector('.task__list-item').cloneNode(true);
+  const textItem = listItem.querySelector('.task__item-text');
+
+  textItem.textContent = text;
+  return listItem;
+}
+
+function formTaskSubmitHandler (evt) {
+  evt.preventDefault();
+  taskListSection.append(createTaskListItem(inputTextTask.value));
+  inputTextTask.value = '';
+}
+
 changeHeightSectionTask();
 
-window.addEventListener('resize', () => { changeHeightSectionTask();});
+window.addEventListener('resize', () => { changeHeightSectionTask(); });
+formAddTask.addEventListener('submit', formTaskSubmitHandler);
